c8a6b9a58428e1c1916f9df180fb0a0c1ca37e79
CaptainGamekiller <yann60810@yahoo.com.tw>
2019-08-20 17:40:12 +0800

    second version

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,11 @@
-x = 3
-while True and x >0:
+i = 3
+while True and i >0:
     password = input('請輸入密碼，最多輸入3次密碼：')
     if password == 'a123456' :
-        print('登入成功')
+        print('登入成功！')
         break#break縮進位置在if的block裡面
     else:
-        x = x-1
-        print('密碼錯誤！還有', x, '次機會')
-        
+        i = i-1
+        print('密碼錯誤！')
+        if i > 0:
+            print('還有', i, '次機會')
