ed6bf2a7a0c11c54211ecd114a0a8ee94638b0de
CaptainGamekiller <53637097+CaptainGamekiller@users.noreply.github.com>
2024-05-01 22:47:30 -0400

    Update password-retry.py

--- password-retry.py
+++ password-retry.py
@@ -1,11 +1,11 @@
 i = 3
-while True and i >0:
-    password = input('請輸入密碼，最多輸入3次密碼：')
-    if password == 'a123456' :
-        print('登入成功！')
-        break#break縮進位置在if的block裡面
+while True and i > 0:
+    password = input('Please input the password. You have up to 3 attempts: ')
+    if password == 'a123456':
+        print('Login successful!')
+        break  
     else:
-        i = i-1
-        print('密碼錯誤！')
+        i = i - 1
+        print('Incorrect password!')
         if i > 0:
-            print('還有', i, '次機會')
+            print('You have', i, 'attempts left.')
